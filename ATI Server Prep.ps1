27055d5bf156e93ddf741ab856008ccf2f93b765
IIDavidJII <52334341+IIDavidJII@users.noreply.github.com>
2019-06-28 14:35:41 -0700

    Add files via upload

---
+++ ATI Server Prep.ps1
@@ -0,0 +1,84 @@
+﻿Configuration ATIServerPrep 
+{
+
+Import-DscResource -ModuleName 'PSDesiredStateConfiguration','NetworkingDSC' , 'xSystemSecurity', 'cDTC', 'ComputerManagementDsc'
+
+#TimeZone
+    TimeZone SetTimeZone
+     {
+       IsSingleInstance = 'Yes'
+       TimeZone = 'Pacific Standard Time'
+     }
+
+#install required windows features
+
+    WindowsFeature MessageQueuing 
+     {
+      Name = "MSMQ"
+      Ensure = "Present"
+     }
+    WindowsFeature MessageQueuingService 
+     {
+      Name = "MSMQ-Services"
+      Ensure = "Present"
+     }
+
+    WindowsFeature MessageQueingServer
+     {
+      Name = "MSMQ-Server"
+      Ensure = "Present"
+     }
+
+#User Account Control    
+    xUac DisableUAC 
+     {
+      Setting ='NeverNotifyAndDisableAll'
+     }
+
+#Distributed Transactions     
+     cDTCNetworkSetting DistributedTrans 
+     {
+       DtcName = "Local"
+       RemoteClientAccessEnabled = $true
+       RemoteAdministrationAccessEnabled = $true
+       InboundTransactionsEnabled = $true
+       OutboundTransactionsEnabled = $true
+       AuthenticationLevel = "NoAuth"
+     }
+
+#Power Plan
+     PowerPlan SetPlanHighPerformance
+        {
+          IsSingleInstance = 'Yes'
+          Name             = 'High performance'
+        }
+
+#IP6 Disable
+      NetAdapterRsc DisableIPV6
+       {
+            Name = 'Ethernet'
+            Protocol = 'IPv6'
+            State = $false 
+       }
+
+#IPV4 disable nic Power Management
+     Script DisablePowerManagement
+       {
+         TestScript = {
+                        $adapterPower = Get-NetAdapterPowerManagement
+                        
+                        If (
+                             $adapterPower.ArpOffload -eq "Unsupported"  -and $adapterPower.NSOffload -eq "Unsupported" -and $adapterPower.RsnRekeyOffload -eq "Unsupported" -and $adapterPower.D0PacketCoalescing-eq "Unsupported" -and $adapterPower.SelectiveSuspend -eq "Unsupported" -and $adapterPower.DeviceSleepOnDisconnect -eq "Unsupported" -and $adapterPower.WakeOnMagicPacket -eq "Unsupported" -and $adapterPower.WakeOnPattern -eq "Unsupported"
+                           ){return $true}
+                             Else {return $false}
+                      }
+
+         SetScript = {$adapters = Get-NetAdapter
+                      FOREACH ($adapter in $adapters){Disable-NetAdapterPowerManagement -Name $adapter.Name}
+                     }
+
+         GetScript = {Get-NetAdapterPowerManagement}
+       }
+      
+}
+
